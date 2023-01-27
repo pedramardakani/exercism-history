7ac41f19e6bace20a77767d7bab3e1a21056c404
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-01-27 12:32:25 +0330

    Two Fer: add solution with f-strings

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,15 @@
-def two_fer(name):
-    pass
+"""Two-fer or 2-fer is short for two for one."""
+
+
+def two_fer(name: str = "you") -> str:
+    """Given a name, return a string with the message:
+
+        One for name, one for me.
+
+    Where "name" is the given name. However, if the name is missing, return the string:
+
+        One for you, one for me.
+
+    :param name: str - the name of the person to print, with a default value of 'you'.
+    :return: str - the two-fer string with the given name."""
+    return f"One for {name}, one for me."
