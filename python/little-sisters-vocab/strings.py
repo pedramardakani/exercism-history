7fbf43071682612db2aea790aa55466f764fce9a
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-01-31 00:45:26 +0330

    Strings: pass the tests, use 're' module
    Updated the patterns sandwiched in single quotations with double quotations
    since the python linters complained about inconsistency.

--- python/little-sisters-vocab/strings.py
+++ python/little-sisters-vocab/strings.py
@@ -1,17 +1,18 @@
 """Functions for creating, transforming, and adding prefixes to strings."""
 
+from re import search, sub
 
-def add_prefix_un(word):
+
+def add_prefix_un(word: str) -> str:
     """Take the given word and add the 'un' prefix.
 
     :param word: str - containing the root word.
     :return: str - of root word prepended with 'un'.
     """
+    return f"un{word}"
 
-    pass
 
-
-def make_word_groups(vocab_words):
+def make_word_groups(vocab_words: list[str]) -> str:
     """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
 
     :param vocab_words: list - of vocabulary words with prefix in first index.
@@ -25,11 +26,15 @@ def make_word_groups(vocab_words):
     For example: list('en', 'close', 'joy', 'lighten'),
     produces the following string: 'en :: enclose :: enjoy :: enlighten'.
     """
+    prefix = vocab_words[0]
+    prefix_list = " :: ".join([f"{prefix}{word}" for word in vocab_words[1:]])
+    return f"{prefix} :: {prefix_list}"
 
-    pass
+
+CONSONANTS = "aeiou"
 
 
-def remove_suffix_ness(word):
+def remove_suffix_ness(word: str) -> str:
     """Remove the suffix from the word while keeping spelling in mind.
 
     :param word: str - of word to remove suffix from.
@@ -37,11 +42,11 @@ def remove_suffix_ness(word):
 
     For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
     """
+    should_replace_i_with_y = search("(?!aeiou)iness$", word)
+    return f"{word[:-len('iness')]}y" if should_replace_i_with_y else word[:-len("ness")]
 
-    pass
 
-
-def adjective_to_verb(sentence, index):
+def adjective_to_verb(sentence: str, index: int) -> str:
     """Change the adjective within the sentence to a verb.
 
     :param sentence: str - that uses the word in sentence.
@@ -50,5 +55,8 @@ def adjective_to_verb(sentence, index):
 
     For example, ("It got dark as the sun set", 2) becomes "darken".
     """
-
-    pass
+    raw_word = sentence.split()[index]
+    # As the raw word might contain extra punctuations such as "comma" and "period",
+    # let's substitute any non-alphabetic character(s) at the end of the word.
+    word = sub("[^a-zA-Z]*$", "", raw_word)
+    return f"{word}en"
