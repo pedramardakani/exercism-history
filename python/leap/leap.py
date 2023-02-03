6803116bf7d5f5457af2219abae2ef115f920728
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-02-03 14:16:39 +0330

    Leap: refactor

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -7,7 +7,11 @@ def leap_year(year: int) -> bool:
     :param year: int - the queried year.
     :return: bool - if it is a leap year or not."""
     match year:
-        case y if y % 100 == 0 and y % 400 == 0:
+        # The order of the following cases are part of the logic! Please do not change
+        # the ordering without running unit-tests.
+        case y if y % 400 == 0:
+            # If year is divisable by 400 it already is divisable by 100, so we
+            # do not need to check for it explicitely.
             return True
         case y if y % 100 == 0:
             return False
