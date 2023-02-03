a5e252bb2ffea973024597f528031353efc8237f
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-02-03 14:01:20 +0330

    Leap: use simple if-else statements

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,17 @@
-def leap_year(year):
-    pass
+"""The leap year module."""
+
+
+def leap_year(year: int) -> bool:
+    """Given a year, report if it is a leap year.
+
+    :param year: int - the queried year.
+    :return: bool - if it is a leap year or not."""
+    if year % 100 == 0:
+        if year % 400 == 0:
+            return True
+        else:
+            return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
