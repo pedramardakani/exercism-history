2f0856cd72d94bf300d339c80bbab16c5f78f2d0
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-02-03 14:22:21 +0330

    Leap: use pure boolean logic

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -6,16 +6,4 @@ def leap_year(year: int) -> bool:
 
     :param year: int - the queried year.
     :return: bool - if it is a leap year or not."""
-    match year:
-        # The order of the following cases are part of the logic! Please do not change
-        # the ordering without running unit-tests.
-        case y if y % 400 == 0:
-            # If year is divisable by 400 it already is divisable by 100, so we
-            # do not need to check for it explicitely.
-            return True
-        case y if y % 100 == 0:
-            return False
-        case y if y % 4 == 0:
-            return True
-        case _:
-            return False
+    return (year % 100 != 0 and year % 4 == 0) or year % 400 == 0
