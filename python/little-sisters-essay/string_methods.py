b664b1f70210bc07989be89786a4a0f4113103e7
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-02-04 12:19:49 +0330

    Little Sister's Essay: pass all tests

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -1,37 +1,37 @@
 """Functions to help edit essay homework using string manipulation."""
 
 
-def capitalize_title(title):
+def capitalize_title(title: str) -> str:
     """Convert the first letter of each word in the title to uppercase if needed.
 
     :param title: str - title string that needs title casing.
     :return: str - title string in title case (first letters capitalized).
     """
 
-    pass
+    return title.title()
 
 
-def check_sentence_ending(sentence):
+def check_sentence_ending(sentence: str) -> bool:
     """Check the ending of the sentence to verify that a period is present.
 
     :param sentence: str - a sentence to check.
     :return: bool - return True if punctuated correctly with period, False otherwise.
     """
 
-    pass
+    return sentence.endswith('.')
 
 
-def clean_up_spacing(sentence):
+def clean_up_spacing(sentence: str) -> str:
     """Verify that there isn't any whitespace at the start and end of the sentence.
 
     :param sentence: str - a sentence to clean of leading and trailing space characters.
     :return: str - a sentence that has been cleaned of leading and trailing space characters.
     """
 
-    pass
+    return sentence.strip()
 
 
-def replace_word_choice(sentence, old_word, new_word):
+def replace_word_choice(sentence: str, old_word: str, new_word: str) -> str:
     """Replace a word in the provided sentence with a new one.
 
     :param sentence: str - a sentence to replace words in.
@@ -40,4 +40,4 @@ def replace_word_choice(sentence, old_word, new_word):
     :return: str - input sentence with new words in place of old words.
     """
 
-    pass
+    return sentence.replace(old_word, new_word)
