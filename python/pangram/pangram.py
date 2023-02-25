eba4206e65e2b9e2f7996be84e1f6d065c1c0d8c
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-02-25 23:26:15 +0330

    Pangram: README, the set method
    There are three approaches shared on the exercism website. I implemented
    the 'all' approach by myself in 15 minutes (and thought that was cool, then
    I was surprised that the website contained the exact same code that I
    wrote!). But then, the 'set' approach looked much more interesting and
    performant.
    
    Also, update the README file.

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,17 @@
-def is_pangram(sentence):
-    pass
+"""Pangram checker module.
+
+Determine if a sentence is a pangram.
+"""
+
+import string
+
+
+ALPHABET = set(string.ascii_lowercase)
+
+
+def is_pangram(sentence: str) -> bool:
+    """Check if a given sentence contains all English letters.
+
+    :param sentence: str - a string consisting letters `a-z` and case insensitive.
+    :return: bool - return True if sentence is a pangram."""
+    return ALPHABET.issubset(sentence.lower())
