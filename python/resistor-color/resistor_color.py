e46f8b74d4c3041910d022ee86455e35a2c13d72
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-02 22:17:38 +0330

    Resistor Color: pass the test

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,21 @@
-def color_code(color):
-    pass
+"""Resistor colors."""
+
+color_list = ["black", "brown", "red", "orange", "yellow",
+              "green", "blue", "violet", "grey", "white"]
+
+color_table = {name: index for index, name in enumerate(color_list)}
 
 
-def colors():
-    pass
+def color_code(color: str) -> int:
+    """Return index of the color given, or None if not found.
+
+    :param color: str - color to be checked.
+    :return: int - color index."""
+    return color_table.get(color, None)
+
+
+def colors() -> list[str]:
+    """Return all known colors.
+
+    :return: list[str] - the list of all known colors."""
+    return color_list
