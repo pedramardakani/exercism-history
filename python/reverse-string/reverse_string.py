547c8dd3989644d3382ce1b208fd1d02a67a0519
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-02 21:45:10 +0330

    Reverse String: pass the test

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,2 +1,9 @@
-def reverse(text):
-    pass
+"""Reverse a given string."""
+
+
+def reverse(text: str) -> str:
+    """Reverse the given text.
+
+    :param text: str - given string.
+    :return: str - reversed string."""
+    return ''.join(reversed(text))
