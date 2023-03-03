c3560437f8cf4f588134d56f46478fe085004b67
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-03 21:58:28 +0330

    Resistor Color Duo: pass the tests

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,15 @@
-def value(colors):
-    pass
+"""Calculate resistor value by color."""
+
+color_list = ["black", "brown", "red", "orange", "yellow",
+              "green", "blue", "violet", "grey", "white"]
+
+color_table = {name: str(index) for index, name in enumerate(color_list)}
+
+
+def value(colors: list[str, str, ...]) -> int:
+    """Get the first two colors of the list and return the corresponding integer.
+
+    :param colors: list[str, str, ...] - a list of two or more strings.
+    :return: int - the integer resembling the resistor value."""
+    digits = (color_table.get(color) for color in colors[:2])
+    return int(''.join(digits))
