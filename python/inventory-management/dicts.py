235c04b561ea5fd339726643ab6ce745009c0a65
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-03 23:54:19 +0330

    Inventory Management: pass the tests

--- python/inventory-management/dicts.py
+++ python/inventory-management/dicts.py
@@ -1,54 +1,59 @@
 """Functions to keep track and alter inventory."""
 
+itemsType = list[str]
+inventoryType = dict[str, int]
 
-def create_inventory(items):
+
+def create_inventory(items: itemsType) -> inventoryType:
     """Create a dict that tracks the amount (count) of each element on the `items` list.
 
     :param items: list - list of items to create an inventory from.
     :return: dict - the inventory dictionary.
     """
+    return add_items(dict(), items)
 
-    pass
 
-
-def add_items(inventory, items):
+def add_items(inventory: inventoryType, items: itemsType) -> inventoryType:
     """Add or increment items in inventory using elements from the items `list`.
 
     :param inventory: dict - dictionary of existing inventory.
     :param items: list - list of items to update the inventory with.
     :return: dict - the inventory updated with the new items.
     """
+    for item in items:
+        inventory[item] = inventory.get(item, 0) + 1
+    return inventory
 
-    pass
 
-
-def decrement_items(inventory, items):
+def decrement_items(inventory: inventoryType, items: itemsType) -> inventoryType:
     """Decrement items in inventory using elements from the `items` list.
 
     :param inventory: dict - inventory dictionary.
     :param items: list - list of items to decrement from the inventory.
     :return: dict - updated inventory with items decremented.
     """
+    for item in items:
+        if inventory[item] > 0:
+            inventory[item] -= 1
+    return inventory
 
-    pass
 
-
-def remove_item(inventory, item):
+def remove_item(inventory: inventoryType, item: str) -> inventoryType:
     """Remove item from inventory if it matches `item` string.
 
     :param inventory: dict - inventory dictionary.
     :param item: str - item to remove from the inventory.
     :return: dict - updated inventory with item removed. Current inventory if item does not match.
     """
+    updated_inventory = inventory.copy()
+    updated_inventory.pop(item, None)
+    return updated_inventory
 
-    pass
 
-
-def list_inventory(inventory):
+def list_inventory(inventory: inventoryType) -> list[tuple[str, int]]:
     """Create a list containing all (item_name, item_count) pairs in inventory.
 
     :param inventory: dict - an inventory dictionary.
     :return: list of tuples - list of key, value pairs from the inventory dictionary.
     """
-
-    pass
+    return [(item, value) for item, value in inventory.items() if value > 0]
