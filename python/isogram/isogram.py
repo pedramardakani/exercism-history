2c114f8c1dfd50f17d688f6da9a5e71ec6e329ec
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-04 22:31:02 +0330

    Isogram: replace method
    I read this apporach [1] and found it interesting. It would work only if a
    'word' or 'phrase' is passed. This iteration is not taking punctuations
    into account, but only the space and the dash.
    
    [1] https://exercism.org/tracks/python/exercises/isogram/approaches/scrub-replace

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -4,16 +4,11 @@ An isogram (also known as a "non-pattern word") is a word or phrase without
 a repeating letter, however spaces and hyphens are allowed to appear multiple times.
 """
 
-import string
-
 
 def is_isogram(text: str) -> bool:
     """Determine if a word or phrase is an isogram.
 
     :param text: str - given text.
     :return: bool - check if is isogram."""
-    lowered_text = text.lower()
-    for letter in lowered_text:
-        if letter in string.ascii_lowercase and lowered_text.count(letter) > 1:
-            return False
-    return True
+    parsed = text.replace(' ', '').replace('-', '').lower()
+    return len(parsed) == len(set(parsed))
