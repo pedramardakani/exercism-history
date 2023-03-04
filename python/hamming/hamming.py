060cab0f3a12824a8e8b5b6720814c5ae235ed52
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-04 22:41:25 +0330

    Hamming: pass the tests

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,15 @@
-def distance(strand_a, strand_b):
-    pass
+"""Exercism's "hamming" challenge.
+
+If we compare two strands of DNA and count the differences between them we can see 
+how many mistakes occurred. This is known as the "Hamming Distance"."""
+
+
+def distance(strand_a: str, strand_b: str) -> int:
+    """Calculate the Hamming Distance between two DNA strands."""
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+    count = 0
+    for a, b in zip(strand_a, strand_b):
+        if a != b:
+            count += 1
+    return count
