4b3d6c4befe0b923cb67be936da2dbd8930423a4
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-04 22:27:25 +0330

    Isogram: pass the tests

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,19 @@
-def is_isogram(string):
-    pass
+"""Exercism's isogram challenge.
+
+An isogram (also known as a "non-pattern word") is a word or phrase without 
+a repeating letter, however spaces and hyphens are allowed to appear multiple times.
+"""
+
+import string
+
+
+def is_isogram(text: str) -> bool:
+    """Determine if a word or phrase is an isogram.
+
+    :param text: str - given text.
+    :return: bool - check if is isogram."""
+    lowered_text = text.lower()
+    for letter in lowered_text:
+        if letter in string.ascii_lowercase and lowered_text.count(letter) > 1:
+            return False
+    return True
