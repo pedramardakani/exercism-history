6bde2b006123c0d20a73666c0d07bd25d9ddb3c6
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-05 12:50:31 +0330

    Bob: use built-in str.isupper instead
    Until now, I tried figuring out if the prompt is uppercase by a custom
    instruction. However, the neat built-in str.isupper() method abstracts that
    away.

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -11,10 +11,8 @@ def response(hey_bob: str) -> str:
     parsed = hey_bob.strip()
     if parsed == '':
         return "Fine. Be that way!"
-
+    is_yelling = parsed.isupper()
     is_question = parsed.endswith('?')
-    has_letters = any(letter in string.ascii_letters for letter in parsed)
-    is_yelling = has_letters and parsed.upper() == parsed
     if is_yelling and is_question:
         return "Calm down, I know what I'm doing!"
     if is_yelling:
