64cf7bdc12fe16cc0c987ea95d42d78e18c0ac49
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-05 23:29:55 +0330

    RNA Transcription: pass the tests

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,14 @@
-def to_rna(dna_strand):
-    pass
+"""Exercism's RNA Transcription challenge."""
+
+translation_table = str.maketrans("T", "A")
+translation_table.update(str.maketrans("G", "C"))
+translation_table.update(str.maketrans("A", "U"))
+translation_table.update(str.maketrans("C", "G"))
+
+
+def to_rna(dna_strand: str):
+    """Given a DNA strand, return its RNA complement (per RNA transcription).
+
+    :param dna_strand: str - a strand of dna.
+    :return: str - the corresponding rna."""
+    return dna_strand.translate(translation_table)
