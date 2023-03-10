8032056ac4d7b945246d0d990fef4445145586fd
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-10 19:43:37 +0330

    Raindrops: pass the tests

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,18 @@
-def convert(number):
-    pass
+"""The raindrops challenge on the exercism website."""
+
+sounds = {
+    3: "Pling",
+    5: "Plang",
+    7: "Plong",
+}
+
+
+def convert(number: int) -> int | str:
+    result = ""
+    for factor, sound in sounds.items():
+        if number % factor == 0:
+            result += sound
+    if result:
+        return result
+    else:
+        return str(number)
