3c329ddb6ddca089a72f38a984b36a3042bbd1cb
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-10 19:19:58 +0330

    ETL: refactor, replace loops altogether
    Use two sequences of generator expressions to remove the complexities.
    
    https://docs.python.org/3/howto/functional.html#generator-expressions-and-list-comprehensions

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,12 +1,9 @@
 """The Extract parto of ETL challenge (Extract-Transform-Load) on the Exercism website."""
 
-import itertools
-
 
 def transform(legacy_data: dict[int, str]) -> dict[str, int]:
-    parsed_data = dict()
-    for score, letters in legacy_data.items():
-        lowered = map(lambda letter: letter.lower(), letters)
-        parsed_data.update(dict(itertools.zip_longest(lowered, [],
-                                                      fillvalue=score)))
-    return parsed_data
+    return {
+        letter.lower(): score
+        for score in legacy_data
+        for letter in legacy_data[score]
+    }
