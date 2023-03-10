8e440d3349677d403ee4ce5995a0d593c59097db
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-10 18:40:45 +0330

    ETL: pass the tests

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,2 +1,9 @@
-def transform(legacy_data):
-    pass
+"""The Extract parto of ETL challenge (Extract-Transform-Load) on the Exercism website."""
+
+
+def transform(legacy_data: dict[int, str]) -> dict[str, int]:
+    parsed_data = dict()
+    for score, letters in legacy_data.items():
+        for letter in letters:
+            parsed_data.update({letter.lower(): score})
+    return parsed_data
