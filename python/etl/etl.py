a112d5810159e5fb0969f38ccef80c2ded78aadd
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-10 19:33:41 +0330

    ETL: add docstring

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -2,6 +2,10 @@
 
 
 def transform(legacy_data: dict[int, str]) -> dict[str, int]:
+    """Reformat legacy data structure to the new one.
+
+    :param legacy_data: dict[int, str] - score is the key, and letters are the values.
+    :return: dict[str, int] - letters are the keys, and each has a score as value."""
     return {
         letter.lower(): score
         for score, letters in legacy_data.items()
