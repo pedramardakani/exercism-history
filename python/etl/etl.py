c3d0ea0e42322089bb1251055c49c3b2b46035b4
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-10 18:51:59 +0330

    ETL: refactor, replace loop with generator

--- python/etl/etl.py
+++ python/etl/etl.py
@@ -1,9 +1,12 @@
 """The Extract parto of ETL challenge (Extract-Transform-Load) on the Exercism website."""
 
+import itertools
+
 
 def transform(legacy_data: dict[int, str]) -> dict[str, int]:
     parsed_data = dict()
     for score, letters in legacy_data.items():
-        for letter in letters:
-            parsed_data.update({letter.lower(): score})
+        lowered = map(lambda letter: letter.lower(), letters)
+        parsed_data.update(dict(itertools.zip_longest(lowered, [],
+                                                      fillvalue=score)))
     return parsed_data
