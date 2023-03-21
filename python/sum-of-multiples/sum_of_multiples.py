a2893e3c61d364e1b712c44d33958d9d53150774
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-21 23:50:14 +0330

    Sum of Multiples: pass the test

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,11 @@
-def sum_of_multiples(limit, multiples):
-    pass
+"""Exercism's sum of multiples challenge."""
+
+
+def sum_of_multiples(limit: int, multiples: list[int]) -> int:
+    final_set = set()
+    for item in multiples:
+        if item == 0:
+            continue
+        final_set = final_set.union(
+            {item * factor for factor in range(1+limit//item) if item * factor < limit})
+    return sum(final_set)
