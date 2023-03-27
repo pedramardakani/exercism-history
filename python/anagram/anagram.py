d34a37cc1630a969de6df7e7b6ee07be0d395a36
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-27 17:05:39 +0330

    Anagram: refactor, rename variables
    For a cleaner code, use more clear and concise variable names.

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -7,7 +7,7 @@ def find_anagrams(word: str, candidates: set[str]) -> set[str]:
     :param word: str - word to check with.
     :param candidates: set[str] - the candidate set of strings being checked.
     :return: set[str] - the set of anagrams."""
-    lowered_word = word.lower()
-    word_status = sorted(lowered_word)
-    return {item for item in candidates
-            if (lowered_item := item.lower()) != lowered_word and word_status == sorted(lowered_item)}
+    low_word = word.lower()
+    criteria = sorted(low_word)
+    return {cand for cand in candidates
+            if (low_cand := cand.lower()) != low_word and criteria == sorted(low_cand)}
