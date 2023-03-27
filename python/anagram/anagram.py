ce79fb8c0f36b6c7a6c42102513f4b4d4ec0db5d
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-03-27 16:57:47 +0330

    Anagram: refactor, use sorted
    Instead of creating a separate function that counts the letters and
    checks for validity, one can sort the letters. If the sorted letters
    in a word match exactly, they have the same number of each letter.

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,14 +1,6 @@
 """Exercism's anagram challenge."""
 
 
-def get_letter_count(word: str) -> dict[str: int]:
-    """Get a dictionary indicating a case-insensitive count of each letter in a word.
-
-    :param word: str - word to check.
-    :return: dict[str: int] - dictionary with keys of letters and values of count of each letter."""
-    return {letter: word.count(letter) for letter in set(word)}
-
-
 def find_anagrams(word: str, candidates: set[str]) -> set[str]:
     """Return a set containing anagrams of the word.
 
@@ -16,6 +8,6 @@ def find_anagrams(word: str, candidates: set[str]) -> set[str]:
     :param candidates: set[str] - the candidate set of strings being checked.
     :return: set[str] - the set of anagrams."""
     lowered_word = word.lower()
-    word_status = get_letter_count(lowered_word)
+    word_status = sorted(lowered_word)
     return {item for item in candidates
-            if (lowered_item := item.lower()) != lowered_word and word_status == get_letter_count(lowered_item)}
+            if (lowered_item := item.lower()) != lowered_word and word_status == sorted(lowered_item)}
