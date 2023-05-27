85f7b9e8a3f68913f9eed27121324485add53587
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-05-27 22:56:15 +0330

    Flatten Array: pass the tests
    Use generators and PEP380 that introduces "yield from". Inspired from
    Zweedeend's answer on:
    
    https://exercism.org/tracks/python/exercises/flatten-array/solutions/Zweedeend

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,15 @@
-def flatten(iterable):
-    pass
+"""Exercism's flatten array challenge."""
+
+def _flatten_helper(items: list | tuple):
+    for item in items:
+        try:
+            iterator = iter(item)
+        except TypeError as err:
+            if item is not None:
+                yield item
+        else:
+            yield from _flatten_helper(iterator)
+
+def flatten(items: list[int | float | str | list[...]]) -> list[int | float | str]:
+    """Take a nested list and return a single flattened list with all values except nil/null."""
+    return list(_flatten_helper(items))
