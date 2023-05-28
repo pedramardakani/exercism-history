5998902d9a4d30bbda9abb586b46b3207f4cd94f
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-05-28 17:15:25 +0330

    Perfect Numbers: pass the tests
    This is ugly though. I'd like a way that finds the divisors in much
    more efficient way.

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,7 +1,16 @@
-def classify(number):
+def classify(number: int) -> str:
     """ A perfect number equals the sum of its positive divisors.
 
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number <= 0:
+        raise ValueError("Classification is only possible for positive integers.")
+    divisors = (div for div in range(1, number//2 + 1) if number % div == 0)
+    match sum(divisors):
+        case s if s > number:
+            return "abundant"
+        case s if s < number:
+            return "deficient"
+        case s if s == number:
+            return "perfect"
