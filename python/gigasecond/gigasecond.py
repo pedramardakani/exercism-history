6098b117ea674d9fe225e69461c11c8efa3cdd18
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-05-29 17:10:47 +0330

    Gigasecond: pass the tests

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,10 @@
-def add(moment):
-    pass
+import datetime as dt
+
+
+def add(moment: dt.datetime) -> dt.datetime:
+    """Add 10**9 seconds to the 'moment' given.
+
+    :param moment: datetime.datetime - initial moment.
+    :return: datetime.datetime - moment after a gigasecond.
+    """
+    return moment + dt.timedelta(seconds=10**9)
