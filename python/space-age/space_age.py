cff1192bf9b289cfac62614ded32834499aac740
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-05-29 22:24:19 +0330

    Space Age: refactor, add docs, better constants
    Use a dictionary to keep track of the coefficients instead of
    hard-coding them. I decided to use class variables to keep all
    SpaceAge objects in sync.

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,35 +1,49 @@
-
-# In seconds
-EARTH_YEAR = 365.25 * 24 * 60 * 60
+"""Exercism's Space Age Challenge"""
 
 
 class SpaceAge:
+    """Given age in seconds, calculate age on other planets."""
+    # In seconds
+    EARTH_YEAR = 365.25 * 24 * 60 * 60
+
+    # Each orbital period is equals to XXX earth years
+    COEFFICIENT_EARTH = {
+        "MERCURY": 0.2408467,
+        "VENUS": 0.61519726,
+        "EARTH": 1,
+        "MARS": 1.8808158,
+        "JUPITER": 11.862615,
+        "SATURN": 29.447498,
+        "URANUS": 84.016846,
+        "NEPTUNE": 164.79132,
+    }
+
     def __init__(self, seconds: int):
         self.seconds = seconds
 
     def calc(self, coefficient: float) -> int:
-        return round(self.seconds / (coefficient * EARTH_YEAR), 2)
+        return round(self.seconds / (coefficient * self.EARTH_YEAR), 2)
 
     def on_mercury(self) -> int:
-        return self.calc(0.2408467)
+        return self.calc(self.COEFFICIENT_EARTH["MERCURY"])
 
     def on_venus(self) -> int:
-        return self.calc(0.61519726)
+        return self.calc(self.COEFFICIENT_EARTH["VENUS"])
 
     def on_earth(self) -> int:
-        return self.calc(1)
+        return self.calc(self.COEFFICIENT_EARTH["EARTH"])
 
     def on_mars(self) -> int:
-        return self.calc(1.8808158)
+        return self.calc(self.COEFFICIENT_EARTH["MARS"])
 
     def on_jupiter(self) -> int:
-        return self.calc(11.862615)
+        return self.calc(self.COEFFICIENT_EARTH["JUPITER"])
 
     def on_saturn(self) -> int:
-        return self.calc(29.447498)
+        return self.calc(self.COEFFICIENT_EARTH["SATURN"])
 
     def on_uranus(self) -> int:
-        return self.calc(84.016846)
+        return self.calc(self.COEFFICIENT_EARTH["URANUS"])
 
     def on_neptune(self) -> int:
-        return self.calc(164.79132)
+        return self.calc(self.COEFFICIENT_EARTH["NEPTUNE"])
