4e1b94d5b78618510d77a8877fafdbae7d57861a
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-05-29 23:46:47 +0330

    Flatten Array: add docs about generator delegators
    Just a simple comment to point to the PEP380 and generator delegators.

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,5 +1,6 @@
 """Exercism's flatten array challenge."""
 
+
 def _flatten_helper(items: list | tuple):
     for item in items:
         try:
@@ -8,8 +9,12 @@ def _flatten_helper(items: list | tuple):
             if item is not None:
                 yield item
         else:
+            # If the delegated generator returns None, the __next__ method
+            # is called on it automatically. So no "None"s returned. More on
+            # https://peps.python.org/pep-0380/
             yield from _flatten_helper(iterator)
 
-def flatten(items: list[int | float | str | list[...]]) -> list[int | float | str]:
+
+def flatten(items: list[int | float | str | None | list[...]]) -> list[int | float | str]:
     """Take a nested list and return a single flattened list with all values except nil/null."""
     return list(_flatten_helper(items))
