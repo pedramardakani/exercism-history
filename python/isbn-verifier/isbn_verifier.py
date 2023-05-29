beeaa0ee7dc9b04876ee70abd5dbf5b57daaa3ed
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-05-29 18:57:46 +0330

    ISBN Verifier: refactor, use simpler logic
    Checking for non-digits is easier and more readable when using "all"
    instead of checking for characters one by one.
    
    Making a list of the initial string is smarter and cleaner. Replacing
    the check digit is also easier this way.
    
    Using a simple "sum" is more succint than a for loop.

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,30 +1,22 @@
 """Exercism's ISBN Verifier challenge."""
 
 
-import string
-
-
 def is_valid(isbn: str) -> bool:
     """Return True if the given ISBN-10 string is valid.
 
     :param isbn: str - given ISBN-10 string, may be hyphenated.
     :return: bool - True if ISBN-10 is valid, False otherwise."""
-    parsed = isbn.replace("-", "")
+    parsed = list(isbn.replace("-", ""))
     # ISBN must be 10 digits
     if len(parsed) != 10:
         return False
-    # Begin calculation by the ISBN verification formula
-    total = 0
-    for index, digit in enumerate(parsed[:-1]):
-        if digit not in string.digits:
-            return False
-        total += int(digit) * (10 - index)
-    check_digit = parsed[-1]
-    match parsed[-1]:
-        case digit if digit.lower() == "x":
-            total += 10
-        case digit if digit in string.digits:
-            total += int(digit)
-        case _:
-            return False
+    # Validate the check digit
+    if parsed[-1].lower() == "x":
+        parsed[-1] = "10"
+    # Discard ISBN if contains non-digits
+    if not all((char.isdigit() for char in parsed)):
+        return False
+    # Calculate by the ISBN verification formula
+    total = sum((int(digit) * (10 - index)
+                for index, digit in enumerate(parsed)))
     return total % 11 == 0
