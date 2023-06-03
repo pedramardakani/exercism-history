819d0781c237ae8776559bf1d7fbdaf37cdc0ba0
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-04 03:04:08 +0330

    Wordy: refactor, better formatting, add docs

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -2,32 +2,41 @@
 
 
 def answer(question: str) -> int | float:
+    # Check for a valid question
     if not question.startswith("What is"):
         raise ValueError("unknown operation")
-    expression = question.removeprefix("What is ").replace("?", "").replace(
-        "plus", "+").replace("divided by", "/").replace("multiplied by", "*").replace("minus", "-")
-    print(f"'{expression}'")
-    splitted = expression.split()
-    iterator = iter(splitted)
+    # Clean up and parse the expression
+    expression = question \
+        .removesuffix("?") \
+        .replace("plus", "+") \
+        .replace("minus", "-") \
+        .removeprefix("What is ") \
+        .replace("divided by", "/") \
+        .replace("multiplied by", "*")
+    iterator = iter(expression.split())
+    # Force evaluation from left to right
     a = next(iterator)
     if not a.removeprefix("-").removeprefix("+").isdigit():
         raise ValueError("syntax error")
     while True:
-        # Check for valid operator
+        # Check for next operator
         try:
             operator = next(iterator)
         except StopIteration:
             break
+        # Check for valid operator
         if operator.isdigit():
             raise ValueError("syntax error")
         if operator not in "+-*/":
             raise ValueError("unknown operation")
-        # Check for valid operand
+        # Check for next operand
         try:
             operand = next(iterator)
         except StopIteration:
             raise ValueError("syntax error")
+        # Check for valid operand which might be negative or positive
         if not operand.removeprefix("-").removeprefix("+").isdigit():
             raise ValueError("syntax error")
+        # Evaluate the expression so far
         a = eval(' '.join((str(a), operator, operand,)))
     return int(a)
