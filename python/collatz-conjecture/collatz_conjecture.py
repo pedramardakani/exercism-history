42a7c6e68cf49fdd22b79351a6ea7f9a25e82abc
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-03 21:51:44 +0330

    Collatz Conjecture: use ternary

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -13,9 +13,6 @@ def steps(number: int) -> int:
     count = 0
     num = number
     while num != 1:
-        if num % 2 == 0:
-            num *= 0.5
-        else:
-            num = 3 * num + 1
         count += 1
+        num = 0.5 * num if num % 2 == 0 else 3 * num + 1
     return count
