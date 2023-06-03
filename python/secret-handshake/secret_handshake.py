34b62a2582f43d599f22ee698a849a8b8a233b88
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-04 00:47:58 +0330

    Secret Handshake: pass the tests

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,11 @@
-def commands(binary_str):
-    pass
+"""Exercism's Secret Handshake Challenge."""
+
+actions = ("wink", "double blink", "close your eyes", "jump", "reverse")
+
+
+def commands(binary_str: str) -> list[str]:
+    queue = [a for a, c in zip(actions, reversed(binary_str)) if c == "1"]
+    if "reverse" in queue:
+        queue.remove("reverse")
+        queue.reverse()
+    return queue
