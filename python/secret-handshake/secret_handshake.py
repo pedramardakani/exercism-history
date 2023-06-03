01094ecb43c2367e9192a81491f100ff116c09df
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-04 00:50:27 +0330

    Secret Handshake: refactor, no need to "remove"

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,11 +1,10 @@
 """Exercism's Secret Handshake Challenge."""
 
-actions = ("wink", "double blink", "close your eyes", "jump", "reverse")
+actions = ("wink", "double blink", "close your eyes", "jump")
 
 
 def commands(binary_str: str) -> list[str]:
     queue = [a for a, c in zip(actions, reversed(binary_str)) if c == "1"]
-    if "reverse" in queue:
-        queue.remove("reverse")
+    if binary_str[0] == "1":
         queue.reverse()
     return queue
