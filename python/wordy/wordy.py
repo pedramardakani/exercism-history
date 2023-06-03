2b30bbebc98aa6b1ced749cf8707f2f6e1ea2689
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-04 02:47:14 +0330

    Wordy: pass the tests
    This passes the tests. But I really don't like how it looks. Let's see
    how others have done it and get some inspiration before moving to the
    next challenge.

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,2 +1,33 @@
-def answer(question):
-    pass
+"""Exercism's Wordy Challenge."""
+
+
+def answer(question: str) -> int | float:
+    if not question.startswith("What is"):
+        raise ValueError("unknown operation")
+    expression = question.removeprefix("What is ").replace("?", "").replace(
+        "plus", "+").replace("divided by", "/").replace("multiplied by", "*").replace("minus", "-")
+    print(f"'{expression}'")
+    splitted = expression.split()
+    iterator = iter(splitted)
+    a = next(iterator)
+    if not a.removeprefix("-").removeprefix("+").isdigit():
+        raise ValueError("syntax error")
+    while True:
+        # Check for valid operator
+        try:
+            operator = next(iterator)
+        except StopIteration:
+            break
+        if operator.isdigit():
+            raise ValueError("syntax error")
+        if operator not in "+-*/":
+            raise ValueError("unknown operation")
+        # Check for valid operand
+        try:
+            operand = next(iterator)
+        except StopIteration:
+            raise ValueError("syntax error")
+        if not operand.removeprefix("-").removeprefix("+").isdigit():
+            raise ValueError("syntax error")
+        a = eval(' '.join((str(a), operator, operand,)))
+    return int(a)
