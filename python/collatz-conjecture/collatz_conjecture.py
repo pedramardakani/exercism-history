a5dfab81981cee5111f9995e24d80f2135d94a1e
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-03 21:45:30 +0330

    Collatz Conjecture: pass the tests

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,15 @@
-def steps(number):
-    pass
+"""Exercism's Collatz Conjecture Challenge."""
+
+
+def steps(number: int):
+    if type(number) is not int or number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    count = 0
+    num = number
+    while num != 1:
+        if num % 2 == 0:
+            num *= 0.5
+        else:
+            num = 3 * num + 1
+        count += 1
+    return count
