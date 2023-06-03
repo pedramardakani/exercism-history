604b2812533a60d313af603c33c123b352f8256c
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-03 22:16:14 +0330

    Collatz Conjecture: refactor
    Until now, the program only counted the number of steps it takes to
    reach 1.
    
    With this commit, the generator yields the series of numbers until it
    reaches 1. This solution is more general and the generator function is
    reusable.

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,6 +1,14 @@
 """Exercism's Collatz Conjecture Challenge."""
 
 
+def generate_collatz_series(number: int) -> int:
+    """Yield the steps of iteration in Collatz Conjecture"""
+    num = number
+    while num != 1:
+        num = 0.5 * num if num % 2 == 0 else 3 * num + 1
+        yield num
+
+
 def steps(number: int) -> int:
     """ Return the number of steps it takes to reach 1 from any given positive
     integer based on the Collatz cojecture.
@@ -10,9 +18,4 @@ def steps(number: int) -> int:
     """
     if type(number) is not int or number <= 0:
         raise ValueError("Only positive integers are allowed")
-    count = 0
-    num = number
-    while num != 1:
-        count += 1
-        num = 0.5 * num if num % 2 == 0 else 3 * num + 1
-    return count
+    return len(tuple(generate_collatz_series(number)))
