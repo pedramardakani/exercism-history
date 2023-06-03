99b68b1c4cfe739ac13461430ce78f0f1c2736fb
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-04 00:59:33 +0330

    Secret Handshake: add docs

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -5,6 +5,7 @@ actions = ("wink", "double blink", "close your eyes", "jump")
 
 def commands(binary_str: str) -> list[str]:
     queue = [a for a, c in zip(actions, reversed(binary_str)) if c == "1"]
+    # The first bit means "reversed"
     if binary_str[0] == "1":
         queue.reverse()
     return queue
