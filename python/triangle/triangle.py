7e313785fe9e739616ed85ea75b712dd92e5314c
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-04 14:21:24 +0330

    Triangle: use decorators
    But the previous approach looked more efficient. Let's just use this
    as an exercise on using decorators.

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,26 +1,40 @@
 """Exercism's Triangle Challenge."""
 
+from typing import Callable
 
-def istriangle(sides: list[int | float]) -> bool:
+side_t = list[int | float]
+
+
+def istriangle(sides: side_t) -> bool:
     if len(sides) != 3 or not all(sides):
         return False
-    sorted_sides = sorted(sides)
-    return sum(sorted_sides[:2]) >= sorted_sides[2]
+    ss = sorted(sides)
+    return sum(ss[:2]) >= ss[2]
 
 
-def equilateral(sides: list[int | float]) -> bool:
-    if istriangle(sides) and len(set(sides)) == 1:
-        return True
-    return False
+def check_triangle(func: Callable) -> Callable | False:
+    """A decorator that checks for trianlges.
+
+    If the given sides cannot form a triangle, return False. Let the function
+    proceed otherwise.
+    """
+    def wrapper(*args):
+        if istriangle(*args):
+            return func(*args)
+        return False
+    return wrapper
 
 
-def isosceles(sides: list[int | float]) -> bool:
-    if istriangle(sides) and len(set(sides)) < 3:
-        return True
-    return False
+@check_triangle
+def equilateral(sides: side_t) -> bool:
+    return len(set(sides)) == 1
 
 
-def scalene(sides: list[int | float]) -> bool:
-    if istriangle(sides) and len(set(sides)) == 3:
-        return True
-    return False
+@check_triangle
+def isosceles(sides: side_t) -> bool:
+    return len(set(sides)) < 3
+
+
+@check_triangle
+def scalene(sides: side_t) -> bool:
+    return len(set(sides)) == 3
