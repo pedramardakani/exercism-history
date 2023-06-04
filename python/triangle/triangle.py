e09b48bb2b950368a9b228df6f2213a8283f05f0
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-04 13:16:02 +0330

    Triangle: pass the tests

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,26 @@
-def equilateral(sides):
-    pass
+"""Exercism's Triangle Challenge."""
 
 
-def isosceles(sides):
-    pass
+def istriangle(sides: list[int | float]) -> bool:
+    if len(sides) != 3 or not all(sides):
+        return False
+    sorted_sides = sorted(sides)
+    return sum(sorted_sides[:2]) >= sorted_sides[2]
 
 
-def scalene(sides):
-    pass
+def equilateral(sides: list[int | float]) -> bool:
+    if istriangle(sides) and len(set(sides)) == 1:
+        return True
+    return False
+
+
+def isosceles(sides: list[int | float]) -> bool:
+    if istriangle(sides) and len(set(sides)) < 3:
+        return True
+    return False
+
+
+def scalene(sides: list[int | float]) -> bool:
+    if istriangle(sides) and len(set(sides)) == 3:
+        return True
+    return False
