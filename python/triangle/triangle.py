542109575efd4d5b15d8af1db7e30e422b882f21
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-04 14:52:14 +0330

    Triangle: refactor, only boolean logic
    Remove all the unnecessary "if" statements.

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -6,19 +6,13 @@ side_t = list[int | float]
 
 
 def istriangle(sides: side_t) -> bool:
-    if (len(sides) != 3
-            or not all((s > 0 for s in sides))
-            or sum(sides) < 2 * max(sides)):
-        return False
-    return True
+    return (len(sides) == 3
+            and all((s > 0 for s in sides))
+            and sum(sides) >= 2 * max(sides))
 
 
 def check_triangle(func: Callable) -> Callable | False:
-    """A decorator that checks for trianlges.
-
-    If the given sides cannot form a triangle, return False. Let the function
-    proceed otherwise.
-    """
+    """A decorator that checks for triangles."""
     def wrapper(*args):
         return istriangle(*args) and func(*args)
     return wrapper
