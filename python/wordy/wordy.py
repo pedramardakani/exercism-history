03e8b3ab3e348c1e89ffdb69ab00f1cc076eaf79
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-04 11:29:26 +0330

    Wordy: deprecate "eval" and use dunder methods
    Eval is dangerous. Let us keep things safe and use dunder methods
    instead.

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,33 +1,43 @@
 """Exercism's Wordy Challenge."""
 
+OPS = {
+    "plus": "__add__",
+    "minus": "__sub__",
+    "multiplied by": "__mul__",
+    "divided by": "__truediv__",
+}
+
+
+def isnumber(duck: str) -> bool:
+    """Digits preceded by a plus or minus sign are valid numbers."""
+    return duck.removeprefix("-").removeprefix("+").isdigit()
+
 
 def answer(question: str) -> int | float:
     # Check for a valid question
     if not question.startswith("What is"):
         raise ValueError("unknown operation")
     # Clean up and parse the expression
-    expression = question \
-        .removesuffix("?") \
-        .replace("plus", "+") \
-        .replace("minus", "-") \
-        .removeprefix("What is ") \
-        .replace("divided by", "/") \
-        .replace("multiplied by", "*")
+    expression = question.removeprefix("What is").removesuffix("?")
+    if not expression:
+        raise ValueError("syntax error")
+    # Replace the words with the actual mathematical operation
+    for name, dunder in OPS.items():
+        expression = expression.replace(name, dunder)
+    print(expression)
+    # The replaced operators must not have whitespace
     iterator = iter(expression.split())
     # Force evaluation from left to right
     a = next(iterator)
-    if not a.removeprefix("-").removeprefix("+").isdigit():
-        raise ValueError("syntax error")
     while True:
         # Check for next operator
         try:
             operator = next(iterator)
         except StopIteration:
             break
-        # Check for valid operator
-        if operator.isdigit():
+        if isnumber(operator):
             raise ValueError("syntax error")
-        if operator not in "+-*/":
+        if operator not in OPS.values():
             raise ValueError("unknown operation")
         # Check for next operand
         try:
@@ -35,8 +45,8 @@ def answer(question: str) -> int | float:
         except StopIteration:
             raise ValueError("syntax error")
         # Check for valid operand which might be negative or positive
-        if not operand.removeprefix("-").removeprefix("+").isdigit():
+        if not isnumber(operand):
             raise ValueError("syntax error")
         # Evaluate the expression so far
-        a = eval(' '.join((str(a), operator, operand,)))
+        a = int(a).__getattribute__(operator)(int(operand))
     return int(a)
