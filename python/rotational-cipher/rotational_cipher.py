6482049a9fe17eb03226a1808ffed29dfcefc20d
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-14 22:54:38 +0330

    Rotational Cipher: refactor
    Use a constant instead of calling "ord" multiple times. Update
    variable names.

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -2,6 +2,11 @@
 
 import string
 
+MAX_UPPERCASE = ord('Z')
+MIN_UPPERCASE = ord('A')
+MAX_LOWERCASE = ord('z')
+MIN_LOWERCASE = ord('a')
+
 
 def rotate(text: str, key: int) -> str:
     initial = list(text)
@@ -9,11 +14,11 @@ def rotate(text: str, key: int) -> str:
     def helper(i: str) -> str:
         if i not in string.ascii_letters:
             return i
-        new = ord(i) + key
-        if i.isupper() and new > ord('Z'):
-            new = new - ord('Z') + ord('A') - 1
-        if i.islower() and new > ord('z'):
-            new = new - ord('z') + ord('a') - 1
-        return chr(new)
+        rotated = ord(i) + key
+        if i.isupper() and rotated > MAX_UPPERCASE:
+            rotated -= MAX_UPPERCASE - MIN_UPPERCASE + 1
+        if i.islower() and rotated > MAX_LOWERCASE:
+            rotated -= MAX_LOWERCASE - MIN_LOWERCASE + 1
+        return chr(rotated)
 
     return ''.join(list(map(helper, initial)))
