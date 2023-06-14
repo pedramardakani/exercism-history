d7cc225e94c50cc35e86c9fda8ebbc54dfa575bc
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-14 22:46:51 +0330

    Rotational Cipher: pass the tests

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,19 @@
-def rotate(text, key):
-    pass
+"""Exercism's rotational cypher challenge (aka Caesar Cypher)"""
+
+import string
+
+
+def rotate(text: str, key: int) -> str:
+    initial = list(text)
+
+    def helper(i: str) -> str:
+        if i not in string.ascii_letters:
+            return i
+        new = ord(i) + key
+        if i.isupper() and new > ord('Z'):
+            new = new - ord('Z') + ord('A') - 1
+        if i.islower() and new > ord('z'):
+            new = new - ord('z') + ord('a') - 1
+        return chr(new)
+
+    return ''.join(list(map(helper, initial)))
