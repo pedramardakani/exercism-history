8b7be76f9c667f786e4889c4cefb738a7581ba78
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-17 20:12:35 +0330

    List-ops: fix type hinting
    A bad guess, typing.Callable is not the same as "callable".

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,39 +1,41 @@
 """Exercism's List-ops challenge."""
 
+import typing
+
 
 def append(items_1: list, items_2: list) -> list:
     return [*items_1, *items_2]
 
 
-def _concat_helper(lists: list):
+def _concat_helper(lists: list) -> typing.Any:
     for item in lists:
         yield from item
 
 
-def concat(lists: list):
+def concat(lists: list) -> list:
     return list(_concat_helper(lists))
 
 
-def filter(function: callable, items: list) -> list:
+def filter(function: typing.Callable, items: list) -> list:
     return [item for item in items if function(item)]
 
 
-def length(items: list):
+def length(items: list) -> int:
     return len(items)
 
 
-def map(function: callable, items: list) -> list:
+def map(function: typing.Callable, items: list) -> list:
     return [function(item) for item in items]
 
 
-def foldl(function: callable, items: list, initial: int | float) -> int | float:
+def foldl(function: typing.Callable, items: list, initial: int | float) -> int | float:
     result = initial
     for item in items:
         result = function(result, item)
     return result
 
 
-def foldr(function: callable, items: list, initial: int | float) -> int | float:
+def foldr(function: typing.Callable, items: list, initial: int | float) -> int | float:
     return foldl(function, reversed(items), initial)
 
 
