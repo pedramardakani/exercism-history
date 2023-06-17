dfac79223d54ceaaacdf9e6e3dcf7328312c96a4
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-17 19:10:40 +0330

    Binary Search: refactor
    Remove extra lines. Use logical shortcuts.

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -3,19 +3,14 @@
 
 def find(search_list: list[int], value: int) -> int:
     """Find an item in a list via binary search method."""
-    if not search_list:
-        raise ValueError("value not in array")
-    a, b = 0, len(search_list)-1
-    mid: int = b // 2
-    while True:
+    low, high = 0, len(search_list)-1
+    while high >= low:
+        mid = (high + low) // 2
         cur = search_list[mid]
         if cur == value:
             return mid
-        elif b <= a:
-            raise ValueError("value not in array")
         elif cur > value:
-            b = mid - 1
-            mid = (b + a) // 2
+            high = mid - 1
         else:
-            a = mid + 1
-            mid = (b + a) // 2
+            low = mid + 1
+    raise ValueError("value not in array")
