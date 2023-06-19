88b635689f0c99b8d7d475a1e9f261da149aa9da
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-19 23:01:00 +0330

    Pig Latin: add comments, ask for feedback
    In case the word is something really wierd, raise an assertion error
    (since my assertions are not met) and let me know of the actual word
    so I can analyze the situation and fix it.

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -5,12 +5,17 @@ import re
 
 def pigify(word: str) -> str:
     """Pig-latin only a single word."""
-    if check := re.match(r'[aieou]|y(?=t)|x(?=r)', word):
+    # Check for consonants first
+    if re.match(r'[aieou]|y(?=t)|x(?=r)', word):
         return word + "ay"
+    # Checking for acceptable vowels. Since we do not know the length of the
+    # vowel group, use the regular expression's "match" function to get the
+    # beginning and end of each match.
     if check := re.match(r'thr|th|ch|sh|sch|qu|rh|[^aieou]', word):
         if word[check.end():].startswith("qu"):
             return word[check.end()+2:] + word[:check.end()+2] + "ay"
         return word[check.end():] + word[:check.end()] + "ay"
+    raise AssertionError("Bug! Please report this word for future analysis!")
 
 
 def translate(text: str) -> str:
