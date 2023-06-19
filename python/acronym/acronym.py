a0a32de4ea60b1a892baca6909c9b1c2f67805f3
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-19 20:43:42 +0330

    Acronym: pass the tests
    Use the regular expressions to have flexibility in splitting the given
    string as suggested by python docs [1].
    
    [1] https://docs.python.org/3/library/re.html

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,9 @@
-def abbreviate(words):
-    pass
+"""Exercism's Acronym Challenge."""
+
+import re
+
+
+def abbreviate(words: str) -> str:
+    cleaned = re.sub(r'[^a-zA-Z\-\s]', "", words)
+    parsed = re.split(r'\W', cleaned)
+    return ''.join((word[0].upper() for word in parsed if word))
