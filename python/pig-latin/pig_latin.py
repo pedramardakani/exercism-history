7eb28c791b3a2b894c10ebc75cf294e4a6a66deb
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-06-19 22:44:47 +0330

    Pig Latin: pass the tests

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,18 @@
-def translate(text):
-    pass
+"""Exercism's Pig Latin Challenge."""
+
+import re
+
+
+def pigify(word: str) -> str:
+    """Pig-latin only a single word."""
+    if check := re.match(r'[aieou]|y(?=t)|x(?=r)', word):
+        return word + "ay"
+    if check := re.match(r'thr|th|ch|sh|sch|qu|rh|[^aieou]', word):
+        if word[check.end():].startswith("qu"):
+            return word[check.end()+2:] + word[:check.end()+2] + "ay"
+        return word[check.end():] + word[:check.end()] + "ay"
+
+
+def translate(text: str) -> str:
+    """Pig-latin an entire text."""
+    return ' '.join((pigify(word) for word in text.split()))
