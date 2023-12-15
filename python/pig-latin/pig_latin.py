c1ea0296348b42e06227ed37b9346d4671cbc65a
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-12-15 12:25:19 +0330

    pig_latin: compile regular expressions

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -2,14 +2,17 @@
 
 import re
 
+RE_CONSONANT = re.compile(r'[aieou]|y(?=t)|x(?=r)')
+RE_VOWELGROUP = re.compile(r'thr|th|ch|sh|sch|qu|rh|[^aieou]')
+
 
 def pigify(word: str) -> str:
     """Pig-latin only a single word."""
     # Check for consonants first
-    if re.match(r'[aieou]|y(?=t)|x(?=r)', word):
+    if RE_CONSONANT.match(word):
         return word + "ay"
     # Get the length of the vowel group
-    check = re.match(r'thr|th|ch|sh|sch|qu|rh|[^aieou]', word)
+    check = RE_VOWELGROUP.match(word)
     if word[check.end():].startswith("qu"):
         return word[check.end()+2:] + word[:check.end()+2] + "ay"
     return word[check.end():] + word[:check.end()] + "ay"
