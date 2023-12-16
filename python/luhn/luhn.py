729830527f552fed4a981edeaaf90744eb3615bd
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-12-17 01:58:29 +0330

    luhn: go math!

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -2,15 +2,11 @@ class Luhn:
     def __init__(self, card_num: str):
         self.original = card_num
         tmp = card_num.replace(" ", "")
-        # Use a tuple so that 'clean' is not mutated unintentionally
         self.clean = () if not tmp.isdigit() else tuple(int(c) for c in tmp)
 
     def valid(self) -> bool:
-        cleaned = list(self.clean)
-        length = len(cleaned)
-        if length < 2:
+        if len(self.clean) < 2:
             return False
-        for i in range(length-2, -1, -2):
-            doubled = 2*cleaned[i]
-            cleaned[i] = doubled - 9*(doubled > 9)
-        return sum(cleaned) % 10 == 0
+        rev = reversed(self.clean)
+        tmp = (x + (i % 2 != 0)*(x-9*(x >= 5)) for i, x in enumerate(rev))
+        return sum(tmp) % 10 == 0
