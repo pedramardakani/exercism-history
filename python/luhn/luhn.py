447983ab79cbd054f425e25595067412e678fa5f
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-12-17 17:31:20 +0330

    luhn: use itertools, get creative

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,11 +1,22 @@
+import itertools as it
+
+
 class Luhn:
     def __init__(self, card_num: str):
         self.original = card_num
         tmp = card_num.replace(" ", "")
         self.clean = () if not tmp.isdigit() else tuple(int(c) for c in tmp)
 
+    def odd_operation(self, x):
+        """Operation that should be done on odd indices"""
+        return x
+
+    def even_operation(self, x):
+        """Operation that should be done on even indices"""
+        return x * 2 - 9 * (x > 4)
+
     def valid(self) -> bool:
+        odd_even = it.cycle((self.odd_operation, self.even_operation))
         return len(self.clean) > 1 and sum(
-            item + (index % 2 != 0) * (item - 9 * (item >= 5))
-            for index, item in enumerate(reversed(self.clean))
+            map(lambda i: next(odd_even)(i), reversed(self.clean))
         ) % 10 == 0
