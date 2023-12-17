595133a6408c37ac4ede9ae42ac3b2514a729cd2
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2023-12-17 16:25:15 +0330

    luhn: refactor
    Always add spacing before and after mathematical operators.
    
    False if COND else EXPRESSION can be shortened to COND and EXPRESSION.

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -5,7 +5,7 @@ class Luhn:
         self.clean = () if not tmp.isdigit() else tuple(int(c) for c in tmp)
 
     def valid(self) -> bool:
-        return False if len(self.clean) < 2 else sum(
-            item + (index % 2 != 0)*(item-9*(item >= 5))
+        return len(self.clean) > 1 and sum(
+            item + (index % 2 != 0) * (item - 9 * (item >= 5))
             for index, item in enumerate(reversed(self.clean))
         ) % 10 == 0
