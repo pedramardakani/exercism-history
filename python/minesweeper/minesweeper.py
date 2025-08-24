ecc770d5d2a4fa433501c51c39870a83d3bf1ec4
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2025-08-24 20:39:03 +0200

    minesweeper: fix step generator logic, raise error

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -6,7 +6,7 @@ def gen_steps(array: list[str], x: int, y: int):
                 continue
             if x+dx < 0 or y+dy < 0:
                 continue
-            if x+dx >= len(array[0]) or y+dy >= len(array):
+            if x+dx >= len(array) or y+dy >= len(array[0]):
                 continue
             steps.append((x+dx, y+dy))
     return steps # yield
@@ -15,9 +15,14 @@ def annotate(minefield):
     annotated = [
         [col == "*" for col in row] for row in minefield
     ]
+    total_cols = len(minefield[0])
     for x, row in enumerate(minefield):
+        if len(row) != total_cols:
+            raise ValueError("The board is invalid with current input.")
         for y, col in enumerate(row):
             steps = gen_steps(minefield, x, y)
+            if minefield[x][y] not in (" ", "*"):
+                raise ValueError("The board is invalid with current input.")
             adjacent_mines = "*" if minefield[x][y] == "*" else sum(minefield[dx][dy] == "*" for (dx,dy) in steps)
             annotated[x][y] = str(adjacent_mines) if adjacent_mines else " "
 
