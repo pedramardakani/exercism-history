822e9cbfd972831e623caa0c776bc50d667b9c9b
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2025-08-24 20:56:01 +0200

    minesweeper: refactor, better names

--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -1,4 +1,5 @@
-def gen_steps(array: list[str], x: int, y: int):
+bad_input_message = "The board is invalid with current input."
+def get_coords(array: list[str], x: int, y: int):
     for dy in (-1, 0, 1):
         for dx in (-1, 0, 1):
             if dy == dx == 0:
@@ -15,18 +16,15 @@ def annotate(minefield):
     ]
     for x, row in enumerate(minefield):
         if len(row) != len(minefield[0]):
-            raise ValueError("The board is invalid with current input.")
-        for y, col in enumerate(row):
-            steps = gen_steps(minefield, x, y)
-            if minefield[x][y] not in (" ", "*"):
-                raise ValueError("The board is invalid with current input.")
-            adjacent_mines = "*" if minefield[x][y] == "*" else sum(minefield[dx][dy] == "*" for (dx,dy) in steps)
+            raise ValueError(bad_input_message)
+        for y, cell in enumerate(row):
+            steps = get_coords(minefield, x, y)
+            if cell not in (" ", "*"):
+                raise ValueError(bad_input_message)
+            adjacent_mines = "*" if cell == "*" else sum(minefield[dx][dy] == "*" for (dx,dy) in steps)
             annotated[x][y] = str(adjacent_mines) if adjacent_mines else " "
 
-    result = [
-        "".join(row) for row in annotated
-    ]
-    return result
+    return ["".join(row) for row in annotated]
 
 
 
