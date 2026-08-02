58595877db3ccddfdc3b9f5d3e181e5a68d041c7
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-08-02 12:22:48 +0200

    go(raindrops): pass

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,20 @@
 package raindrops
 
+import "fmt"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var sound string = ""
+	if number%3 == 0 {
+		sound += "Pling"
+	}
+	if number%5 == 0 {
+		sound += "Plang"
+	}
+	if number%7 == 0 {
+		sound += "Plong"
+	}
+	if sound == "" {
+		sound = fmt.Sprintf("%d", number)
+	}
+	return sound
 }
