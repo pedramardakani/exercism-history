160ff8c90698a7a621762e4554cc0cc260645616
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-08-02 20:41:05 +0200

    go(randrops): remember the mistake
    Iteration order over a map with a range is not deterministic.

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,16 +2,22 @@ package raindrops
 
 import "fmt"
 
+var soundMap = map[int]string{
+	3: "Pling",
+	5: "Plang",
+	7: "Plong",
+}
+
 func Convert(number int) string {
 	var sound string = ""
-	if number%3 == 0 {
-		sound += "Pling"
-	}
-	if number%5 == 0 {
-		sound += "Plang"
-	}
-	if number%7 == 0 {
-		sound += "Plong"
+
+	// DO NOT DO THIS!
+	// Iteration order over a map is not guaranteed to be deterministic!
+	// https://go.dev/blog/maps#iteration-order
+	for k, v := range soundMap {
+		if number%k == 0 {
+			sound += v
+		}
 	}
 	if sound == "" {
 		sound = fmt.Sprintf("%d", number)
