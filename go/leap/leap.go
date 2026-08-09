048f8f25ac396841acc2f3db356764eb676747ea
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-08-09 15:47:58 +0200

    go(leap): refactor, early return

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,17 +1,17 @@
 package leap
 
 func IsLeapYear(year int) bool {
-	// add an extra day every 4 years
-	if year%4 == 0 {
-		// but this becomes 1 day too fast for each 100 years
-		if year%100 == 0 {
-			// address the error of one day in every 400 years
-			if year%400 == 0 {
-				return true
-			}
-			return false
-		}
+	if year % 400 == 0 {
 		return true
 	}
+
+	if year % 100 == 0 {
+		return false
+	}
+
+	if year % 4 == 0 {
+		return true
+	}
+
 	return false
 }
