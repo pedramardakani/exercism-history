6d9b9de77e90bcfd7d239013d51b4b0239bd1ce8
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-08-09 15:49:38 +0200

    go(leap): single boolean statement

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,17 +1,5 @@
 package leap
 
 func IsLeapYear(year int) bool {
-	if year % 400 == 0 {
-		return true
-	}
-
-	if year % 100 == 0 {
-		return false
-	}
-
-	if year % 4 == 0 {
-		return true
-	}
-
-	return false
+	return (year%4 == 0 && !(year%100 == 0)) || (year%400 == 0)
 }
