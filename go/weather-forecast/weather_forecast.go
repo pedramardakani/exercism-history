1baddb467538090bed3d66ce019fbd4eac0710c0
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-08-09 16:44:27 +0200

    go(weather_forecast): pass

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,10 +1,14 @@
+// Package weather creates a string representation of the current weather forecast.
 package weather
 
 var (
+	// CurrentCondition is a string telling the current weather condition.
 	CurrentCondition string
+	// CurrentLocation is a string telling you where this information belongs to.
 	CurrentLocation  string
 )
 
+// Forecast takes the city and current condition and returns a readable string for the teleprompter.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
