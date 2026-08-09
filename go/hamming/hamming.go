c9d8ef62cebb0b874e9420119f7a918439bb3f80
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-08-09 14:45:58 +0200

    go(hamming): range can return only index

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -7,7 +7,7 @@ func Distance(a, b string) (int, error) {
 		return 0, errors.New("DNA strands must have equal length")
 	}
 	difference := 0
-	for index, _ := range a {
+	for index := range a {
 		if a[index] != b[index] {
 			difference++
 		}
