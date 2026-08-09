c5436e01e124c815ac8475139f1ad7830dd5cc2a
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-08-09 14:42:44 +0200

    go(hamming): pass

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,16 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("DNA strands must have equal length")
+	}
+	difference := 0
+	for index, _ := range a {
+		if a[index] != b[index] {
+			difference++
+		}
+	}
+	return difference, nil
 }
