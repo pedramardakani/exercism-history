c552ff9f96888e677ed773bbb7d375e5b3cb7d5d
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-08-09 15:34:26 +0200

    go(leap): pass

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,15 +1,17 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package leap
 
-// IsLeapYear should have a comment documenting it.
 func IsLeapYear(year int) bool {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	panic("Please implement the IsLeapYear function")
+	// add an extra day every 4 years
+	if year%4 == 0 {
+		// but this becomes 1 day too fast for each 100 years
+		if year%100 == 0 {
+			// address the error of one day in every 400 years
+			if year%400 == 0 {
+				return true
+			}
+			return false
+		}
+		return true
+	}
+	return false
 }
