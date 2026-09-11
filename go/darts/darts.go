6041005bee6c37203e616a52622ae3fcca22d73c
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-09-11 23:08:55 +0200

    go(dart): pass

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,5 +1,14 @@
 package darts
 
-func Score(x, y float64) int {
-	panic("Please implement the Score function")
+func Score(x float64, y float64) int {
+	zSquared := x*x + y*y
+	if zSquared > 100 {
+		return 0
+	} else if zSquared > 25 {
+		return 1
+	} else if zSquared > 1 {
+		return 5
+	} else {
+		return 10
+	}
 }
