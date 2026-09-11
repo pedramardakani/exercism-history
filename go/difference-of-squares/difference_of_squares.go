15fd91ecaac76186dfe73fd6d1f3db1bdcbbae2e
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-09-11 21:26:39 +0200

    go(difference_of_squares): pass

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,21 @@
 package differenceofsquares
 
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	sum := 0
+	for i := n; i > 0; i-- {
+		sum += i
+	}
+	return sum * sum
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	sum := 0
+	for i := n; i > 0; i-- {
+		sum += i * i
+	}
+	return sum
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
