cf23a14307cb828f618395e893bf784577ec2158
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-09-11 21:14:23 +0200

    go(collatz_conjecture): pass tests

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,20 @@
 package collatzconjecture
 
+import "fmt"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n < 1 {
+		return 0, fmt.Errorf("expected positive integer, received '%d'", n)
+	}
+	cur := n
+	step := 0
+	for cur != 1 {
+		step += 1
+		if cur%2 == 0 {
+			cur /= 2
+		} else {
+			cur = cur*3 + 1
+		}
+	}
+	return step, nil
 }
