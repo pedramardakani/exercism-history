041357b8447959d3a334cdfcf2bba307c7ca531f
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-09-12 23:29:46 +0200

    go(vehicle_purchase): refactor, use string array

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -2,7 +2,13 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	return kind == "car" || kind == "truck"
+	licensedVehicles := []string{"car", "truck"}
+	for _, vehicle := range licensedVehicles {
+		if vehicle == kind {
+			return true
+		}
+	}
+	return false
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
@@ -11,8 +17,7 @@ func ChooseVehicle(option1, option2 string) string {
 	if option1 < option2 {
 		choice = option1
 	}
-	choice += " is clearly the better choice."
-	return choice
+	return choice + " is clearly the better choice."
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
