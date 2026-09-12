e818fb25ef478df9767a05c012053632289dcf91
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-09-12 23:40:13 +0200

    go(party_robot): init

---
+++ go/party-robot/party_robot_test.go
@@ -0,0 +1,130 @@
+package partyrobot
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestWelcome(t *testing.T) {
+	tests := []struct {
+		description string
+		name        string
+		want        string
+	}{
+		{
+			description: "Greet Chihiro with a welcoming message",
+			name:        "Chihiro",
+			want:        "Welcome to my party, Chihiro!",
+		},
+		{
+			description: "Greet Xuân Jing with a welcoming message",
+			name:        "Xuân Jing",
+			want:        "Welcome to my party, Xuân Jing!",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := Welcome(tc.name); got != tc.want {
+				t.Errorf("Welcome(%q) = %q, want %q", tc.name, got, tc.want)
+			}
+		})
+	}
+}
+
+func TestHappyBirthday(t *testing.T) {
+	tests := []struct {
+		description string
+		name        string
+		age         int
+		want        string
+	}{
+		{
+			description: "Wish Chihiro Happy Birthday with name and age",
+			name:        "Chihiro",
+			age:         61,
+			want:        "Happy birthday Chihiro! You are now 61 years old!",
+		},
+		{
+			description: "Wish Xuân Jing Happy Birthday with name and age",
+			name:        "Xuân Jing",
+			age:         17,
+			want:        "Happy birthday Xuân Jing! You are now 17 years old!",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := HappyBirthday(tc.name, tc.age); got != tc.want {
+				t.Errorf("HappyBirthday(%q, %d) = %q, want %q", tc.name, tc.age, got, tc.want)
+			}
+		})
+	}
+}
+
+func TestAssignTable(t *testing.T) {
+	tests := []struct {
+		description string
+		name        string
+		direction   string
+		tableNumber int
+		distance    float64
+		seatmate    string
+		want        string
+	}{
+		{
+			description: "Greet Chihiro and give them directions to their seat",
+			name:        "Chihiro",
+			direction:   "straight ahead",
+			tableNumber: 22,
+			distance:    9.2394381,
+			seatmate:    "Akachi Chikondi",
+			want:        "Welcome to my party, Chihiro!\nYou have been assigned to table 022. Your table is straight ahead, exactly 9.2 meters from here.\nYou will be sitting next to Akachi Chikondi.",
+		},
+		{
+			description: "Greet Xuân Jing and give them directions to their seat",
+			name:        "Xuân Jing",
+			direction:   "by the façade",
+			tableNumber: 4,
+			distance:    23.470103,
+			seatmate:    "Renée",
+			want:        "Welcome to my party, Xuân Jing!\nYou have been assigned to table 004. Your table is by the façade, exactly 23.5 meters from here.\nYou will be sitting next to Renée.",
+		},
+		{
+			description: "Greet Paula and give them directions to their seat",
+			name:        "Paula",
+			direction:   "on the right",
+			tableNumber: 101,
+			distance:    100.0,
+			seatmate:    "Chioma",
+			want:        "Welcome to my party, Paula!\nYou have been assigned to table 101. Your table is on the right, exactly 100.0 meters from here.\nYou will be sitting next to Chioma.",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := AssignTable(tc.name, tc.tableNumber, tc.seatmate, tc.direction, tc.distance); got != tc.want {
+				t.Errorf(
+					"AssignTable(%q, %d, %q, %q, %f),\ngot: %q\nwant: %q",
+					tc.name, tc.tableNumber, tc.seatmate, tc.direction, tc.distance, got, tc.want,
+				)
+				wantLen := len(tc.want)
+				gotLen := len(got)
+				wantNewlines := strings.Count(tc.want, "\n")
+				gotNewlines := strings.Count(got, "\n")
+				wantSpaces := strings.Count(tc.want, " ")
+				gotSpaces := strings.Count(got, " ")
+
+				if wantLen != gotLen {
+					t.Errorf("String lengths do not match - got: %d, want: %d", gotLen, wantLen)
+				}
+
+				if wantNewlines != gotNewlines {
+					t.Errorf("Check your newline characters - got: %d, want: %d", gotNewlines, wantNewlines)
+				}
+
+				if wantSpaces != gotSpaces {
+					t.Errorf("Check your space characters - got: %d, want: %d", gotSpaces, wantSpaces)
+				}
+
+			}
+		})
+	}
+}
