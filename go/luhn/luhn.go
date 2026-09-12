cbcf66c0c60780b96ff0ca2624980b488d2e9d48
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-09-12 16:04:43 +0200

    go(luhn): handle errors explicitly

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,7 +1,9 @@
 package luhn
 
+import "fmt"
+
 // normalizeStringOfDigits - Normalize a string of digits, return empty string if invalid
-func normalizeStringOfDigits(id string) []int {
+func normalizeStringOfDigits(id string) ([]int, error) {
 	parsed := make([]int, 0)
 	for _, character := range id {
 		// empty whitespace is tolerated
@@ -10,11 +12,16 @@ func normalizeStringOfDigits(id string) []int {
 		}
 		// non-digit characters are not tolerated
 		if character < '0' || character > '9' {
-			return nil
+			return nil, fmt.Errorf("Expected digits, received '%c'", character)
 		}
 		parsed = append(parsed, int(character-'0'))
 	}
-	return parsed
+
+	if nDigits := len(parsed); nDigits <= 1 {
+		return nil, fmt.Errorf("At least two digits expected, received '%d'", nDigits)
+	}
+
+	return parsed, nil
 }
 
 // prepareLuhnDigit - Prepare the digit to be calculated in the Luhn sum.
@@ -27,12 +34,12 @@ func prepareLuhnDigit(digit int) int {
 }
 
 func Valid(id string) bool {
-	parsedId := normalizeStringOfDigits(id)
-	nDigits := len(parsedId)
-	if nDigits < 2 {
+	parsedId, err := normalizeStringOfDigits(id)
+	if err != nil {
 		return false
 	}
 
+	nDigits := len(parsedId)
 	sum := 0
 	for index, digit := range parsedId {
 		isSecond := (nDigits-index)%2 == 0
