ff40f6603c15c762cd3bcf4486ac05275cd0b9d6
Pedram Ashofteh-Ardakani <pedramardakani@pm.me>
2026-09-13 01:06:03 +0200

    go(phone_number): pass

--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -1,13 +1,53 @@
 package phonenumber
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Number(phoneNumber string) (string, error) {
-	panic("Please implement the Number function")
+	acceptablePunctuation := "-.() "
+	maxDigits := 10
+	normalized := ""
+	position := 0
+	for index := len(phoneNumber) - 1; index >= 0; index-- {
+		character := phoneNumber[index]
+		if strings.Contains(acceptablePunctuation, string(character)) {
+			continue
+		}
+		if position == maxDigits && (character == '+' || character == '1') {
+			continue
+		}
+		if position >= maxDigits {
+			return "", fmt.Errorf("Expected %d digits, received more. Pos %d rune %c", maxDigits, position, character)
+		}
+		if (position == 6 || position == 9) && character < '2' {
+			return "", fmt.Errorf("Invalid Area code/Local number, got '%c'", character)
+		}
+		if character < '0' || character > '9' {
+			return "", fmt.Errorf("Expected digits, received '%c'.", character)
+		}
+		normalized = string(character) + normalized
+		position++
+	}
+	if nDigits := len(normalized); nDigits != maxDigits {
+		return "", fmt.Errorf("Expected %d digits, received '%d'", maxDigits, nDigits)
+	}
+	return normalized, nil
 }
 
 func AreaCode(phoneNumber string) (string, error) {
-	panic("Please implement the AreaCode function")
+	number, err := Number(phoneNumber)
+	if err != nil {
+		return "", err
+	}
+	return number[:3], nil
 }
 
 func Format(phoneNumber string) (string, error) {
-	panic("Please implement the Format function")
+	number, err := Number(phoneNumber)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("(%s) %s-%s", number[:3], number[3:6], number[6:]), nil
 }
